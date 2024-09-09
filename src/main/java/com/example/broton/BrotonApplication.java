0b93eb4932eb1681329465fd00abbf4c4d6a61e0
Jasmine Broton <jbroton@emich.edu>
2024-09-09 13:37:20 -0400

    Initial commit

---
+++ src/main/java/com/example/broton/BrotonApplication.java
@@ -0,0 +1,13 @@
+package com.example.broton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrotonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrotonApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/broton/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.broton;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String getGreeting() {
+        return "Hi it's me and I like pizza";
+    }
+}
